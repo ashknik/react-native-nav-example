f08101824293d835966c58d949932d94be20fabc
Nasser Al-ALi <nasseral-ali@Nassers-Mac-mini.local>
2022-02-10 16:33:20 +0400

    Resolved Issues

--- Screens/AppStack.js
+++ Screens/AppStack.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { AuthContext } from '../components/AuthContext';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import HomeScreen from './HomeScreen';
 import { TextInput } from 'react-native-gesture-handler';
 
@@ -30,72 +31,75 @@ const CreateInvite = () => {
   console.log(location.length);
 
   return (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
 
-      {
-        location.length == 0 &&
+    <ScrollView showsVerticalScrollIndicator={false} horizontal={false} style={{ flex: 1, width: '100%', height: '100%' }}>
+      <ScrollView horizontal={true} style={{ flex: 1, width: 350, height: 350 }}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+
+          {
+            location.length == 0 &&
+            <View style={{ flex: 4, padding: 15 }} >
+              <GooglePlacesAutocomplete
+
+                styles={{
+                  container: {
+                    width: '100%',
+                  },
+                  textInputContainer: {
+                    width: '100%',
+                  },
+                  textInput: {
+                    width: '100%',
+                  }
+                }}
+                placeholder='Select a place'
+                listViewDisplayed={false}
+                onPress={(data, details = null) => {
+                  // 'details' is provided when fetchDetails = true
+                  setLocation(details.structured_formatting.main_text)
+                  //setLocation(details.description)
+                  // console.log(details);
+                }}
+                query={{
+                  key: 'AIzaSyBQHKZTwhPJX_9IevM5jKC8kmz0NzqAaBk',
+                  language: 'en',
+                }}
+              />
+            </View>
+          }
+
+          {
+            location.length > 0 &&
+
+            <View style={{ flex: 6, padding: 15, height: 90, }} >
+              <TextInput value={location} enabled={false} multiline={true}
+                numberOfLines={2}></TextInput>
+
+            </View>
 
 
-        <View style={{ flex: 4, padding: 15 }} >
-          <GooglePlacesAutocomplete
-            placeholder='Select a place'
-            onPress={(data, details = null) => {
-              // 'details' is provided when fetchDetails = true
-               setLocation(details.structured_formatting.main_text)
-              //setLocation(details.description)
-              // console.log(details);
-            }}
-            query={{
-              key: 'AIzaSyBQHKZTwhPJX_9IevM5jKC8kmz0NzqAaBk',
-              language: 'en',
-            }}
-          />
+          }
+
+          {
+            location.length > 0 &&
+            <TouchableOpacity style={{ flex: 2, padding: 25, height: 72, }} onPress={() => { setLocation('') }} >
+              <MaterialIcons name="edit" size={23} />
+            </TouchableOpacity>
+
+          }
+
+
+
+
+
         </View>
-      }
 
-      {
-        location.length > 0 &&
-
-        <View style={{ flex: 6, padding: 15, height: 90, }} >
-          <TextInput value={location} enabled={false} multiline={true}
-    numberOfLines={2}></TextInput>
-
-        </View>
-
-
-      }
-
-      {
-        location.length > 0 &&
-        <TouchableOpacity style={{ flex: 2, padding: 25, height: 72, }} onPress={()=>{setLocation('')}} >
-          <MaterialIcons name="edit" size={23} />
-        </TouchableOpacity>
-
-      }
+      </ScrollView>
+    </ScrollView>
 
 
 
 
-      {/* {location.length > 0 ?
-
-        <>
-          <View style={{ flex: 2, padding: 15, height: 62, justifyContent: 'flex-end' }} >
-
-          {location &&
-            <View>  {location} </View>
-            }
-
-          </View>
-          <View style={{ flex: 2, padding: 15, height: 62, justifyContent: 'flex-end' }} >
-
-            <TouchableOpacity>   <MaterialIcons name="upload-file" size={23} />  </TouchableOpacity>
-
-          </View>
-        </> : null
-
-      } */}
-
-    </View>
   )
 }
 
